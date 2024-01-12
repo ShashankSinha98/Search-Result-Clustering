bd95c59f426135ce42879d197230cccca26de765
Shashank Sinha <sinhashashank.98@gmail.com>
2024-01-13 00:00:14 +0100

    Remove unused files

--- EasyMath/src/math/CMath.java
+++
@@ -1,9 +0,0 @@
-package math;
-public class CMath {
-	
-	public static int square(int x) throws InterruptedException {
-		System.out.println("square called: "+x);
-		Thread.sleep(2000);
-		return x*x;
-	}
-}
